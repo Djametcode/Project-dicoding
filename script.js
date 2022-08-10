cd04f6ff0e6bb796d01abc5ee77997b62a8a0601
djametcode <kampretsiji1@gmail.com>
2022-08-10 17:24:37 +0700

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("newNav").style.width = "300px" ;
+    document.body.style.marginLeft = "300px";
+}
+function closeNav() {
+    document.getElementById("newNav").style.width = "0" ;
+    document.body.style.marginLeft = "0";
+}
