c4c9a2f0a4a01bb6ccb904891043844aa466bd1d
djametcode <kampretsiji1@gmail.com>
2022-08-10 17:57:23 +0700

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function openNav() {
     document.getElementById("newNav").style.width = "300px" ;
     document.body.style.marginLeft = "300px";
+    document.getElementById("topheader").style.overflow = "hidden" ;
 }
 function closeNav() {
     document.getElementById("newNav").style.width = "0" ;
